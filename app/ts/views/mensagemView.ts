299139345ad7a0477cb869f8cd2dc7da2648a87a
Lucian AF <lucian_af@hotmail.com>
2021-01-25 21:42:14 -0300

    001 - Primeiro commit, subindo arquivos do curso parte 1;

---
+++ app/ts/views/mensagemView.ts
@@ -0,0 +1,7 @@
+import { View } from "./view";
+
+export class MensagemView extends View<string> {
+  template(modelo: string): string {
+    return `<p class="alert alert-info">${modelo}</p>`;
+  }
+}
